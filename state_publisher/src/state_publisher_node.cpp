bbfaa3e2c73b1db87b289e1329f6cf9f48bac83e
Tassos (nakano) Natsakis <891234ser@eipieq.com>
2020-01-08 20:32:15 +0000

    Creating a node for publishing joint states

---
+++ state_publisher/src/state_publisher_node.cpp
@@ -0,0 +1,31 @@
+#include "ros/ros.h"
+#include "robot_msgs/joints.h"
+#include "sstream"
+
+int main(int argc, char **argv)
+{
+  ros::init(argc, argv, "state_publisher");
+  ros::NodeHandle n;
+  ros::Publisher state_pub = n.advertise<robot_msgs::joints>("joint_state", 1000);
+
+  ros::Rate loop_rate(10);
+
+  int count = 0;
+  while (ros::ok())
+  {
+    robot_msgs::joints msg;
+
+    msg.q1.data = cos(count);
+    msg.q2.data = sin(count);
+    msg.q3.data = 0.5*cos(count*2);
+    msg.q4.data = 0.5*cos(count/2);
+
+    state_pub.publish(msg);
+
+    ros::spinOnce();
+
+    loop_rate.sleep();
+    ++count;
+  }
+  return 0;
+}
