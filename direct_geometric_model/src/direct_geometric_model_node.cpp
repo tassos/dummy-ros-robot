65b328d57d1edfadaaec31c259f27690bb0f1afb
Tassos (nakano) Natsakis <891234ser@eipieq.com>
2020-01-08 21:24:33 +0000

    More 'realistic' pose msgs

--- direct_geometric_model/src/direct_geometric_model_node.cpp
+++ direct_geometric_model/src/direct_geometric_model_node.cpp
@@ -1,6 +1,7 @@
 #include "ros/ros.h"
 #include "geometry_msgs/Pose.h"
 #include "robot_msgs/joints.h"
+#include "cmath"
 
 ros::Publisher ee_pose;
 /**
@@ -9,6 +10,13 @@ ros::Publisher ee_pose;
 void chatterCallback(const robot_msgs::joints::ConstPtr& msg)
 {
   geometry_msgs::Pose pose_msg;
+  pose_msg.position.x = msg->q1*2;
+  pose_msg.position.y = msg->q2/2;
+  pose_msg.position.z = msg->q3/4;
+  pose_msg.orientation.x = pow(msg->q1,2);
+  pose_msg.orientation.y = msg->q2;
+  pose_msg.orientation.z = msg->q1-msg->q2;
+  pose_msg.orientation.w = msg->q3;
   ee_pose.publish(pose_msg);
 }
 
@@ -17,7 +25,7 @@ int main(int argc, char **argv)
   ros::init(argc, argv, "direct_geometric_model");
   ros::NodeHandle n;
   ee_pose = n.advertise<geometry_msgs::Pose>("ee_pose", 1000);
-  ros::Subscriber sub = n.subscribe("joints_state", 1000, chatterCallback);
+  ros::Subscriber sub = n.subscribe<robot_msgs::joints>("joints_state", 1000, chatterCallback);
   ros::spin();
 
   return 0;
